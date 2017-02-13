683de9f6790d1305e8c3b506a5cfbf90f7f3269d
Mathias Rav <rav@cs.au.dk>
2017-02-13 12:04:34 +0100

    WIP

--- caching.py
+++ caching.py
@@ -2,6 +2,7 @@ import os
 import json
 import pickle
 import inspect
+import datetime
 import functools
 
 
--- generate_today.py
+++ generate_today.py
@@ -1,3 +1,4 @@
+import re
 import datetime
 import requests
 
@@ -13,10 +14,23 @@ def main():
     rates_visa, t2 = get_rates(session, date_str, issuer='', card='VISA',
                                cache_time=True)
     t = min(t1, t2)
-    print('<p>Date: %s</p>' % date_str)
-    print('<p>MasterCard: %s</p>' % (dict(rates_mc)['USD'],))
-    print('<p>VISA: %s</p>' % (dict(rates_visa)['USD'],))
-    print('<p>Data retrieved: %s</p>' % (t,))
+    with open('today.tpl.html') as fp:
+        tpl = fp.read()
+
+    data = dict(
+        date=date_str,
+        mc=dict(rates_mc)['USD'],
+        visa=dict(rates_visa)['USD'],
+        retrieved=t,
+    )
+
+    def repl(mo):
+        return str(data[mo.group(1)])
+
+    output = re.sub(r'\{\{ ([a-z]+) \}\}', repl, tpl)
+
+    with open('today.html', 'w') as fp:
+        fp.write(output)
 
 
 if __name__ == '__main__':
